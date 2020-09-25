913c9839f8a2616209c218f862d4514a58a0b9e6
Kyle Everett Lang <kyle.everett.lang@gmail.com>
2020-09-25 03:04:58 -0500

    Began work on checkForWin method.

--- javascript/bingo/bingo.js
+++ javascript/bingo/bingo.js
@@ -18,7 +18,7 @@ class Game {
         let tensGenerator = () => (Math.floor(Math.random() * Math.floor(10)) + 1) * 10;
         for(let i = 0; i < 5; i++) {
             let n = tensGenerator();
-            // Checks to make sure no duplicate ranges are added.
+            // Checks to make sure no duplicate ranges are added. Does it, though?
             for( let i = 0; i < ranges.length; i++) { if(ranges[i][1] == n) n = tensGenerator(); }
             ranges.push([n - 10, n]);
         }
@@ -29,22 +29,17 @@ class Game {
         for(let i = 0; i < players.length; i++) { this.boards.push( new Board(this.ranges, players[i]) ); }
     }
 
-    displayBoards() {
-        for(let i = 0; i < this.boards.length; i++) {
-            console.log(this.boards[i].player);
-            console.log(this.boards[i].columns);
-        }
-    }
-
     drawNumber() {
         let column = Math.floor(Math.random() * 5);
         let range = this.ranges[column];
         let n = getRandomIntInRange(range[0], range[1]);
-        // We'll need to save the drawn numbers to a class property and check each draw against what has already been drawn.
+        // Write code to check that n does not exist in this.drawNumbers. Recursive function that checks and calls itself if true.
+        // this.drawnNumbers.push(n);
         console.log("The number drawn was: " + n);
         
         // At the top level we have a Game object with an array of board objects.
         for(let i = 0; i < this.boards.length; i++) {
+            console.log("working on " + this.boards[i].player + "'s board");
             // This loop iterates for the same number as the length of the board array.
             for(let j = 0; j < this.boards[i].columns.length; j++) {
                 // Each board has an array of columns. First select the board.
@@ -52,19 +47,99 @@ class Game {
                 // Then select the square in the column.
                 let square = this.boards[i].columns[column][j];
                 if(square.value == n) {
-                    square.value = 'X';
+                    square.value = 'X'; // Remove this line to keep numbers after being selected.
                     square.status = true;
-                    this.gameWon = true;
                 };
             }
         }
-        this.displayBoards();
+
+        let winner = this.checkForWin();
+        console.log(winner);
+        if(winner) {
+            console.log('This got triggered.');
+            this.gameWon = true;
+        }
+
         if(this.gameWon) {
-            console.log('Congratulations! You win!')
+            this.boards.forEach((board) => {
+                if(board.player == winner) board.displayBoard();
+            })
+            console.log(`Congratulations! ${winner} is the winner!`);
         }
     }
 
+    checkForWin() {
+        const isTrue = (value) => { return value; }
 
+        const checkColumns = (board) => {
+            let columns = board.columns;
+            let check = false;
+
+            for(let i = 0; i < columns.length; i++) {
+                let statuses = columns[i].map((square) => square.status);
+                check = statuses.every(isTrue);
+                if(check) break;
+            }
+            return check;
+        } 
+
+        const checkRows = (board) => {
+            let columns = board.columns;
+            let rows = [[], [], [], [], []];
+            let check = false;
+
+            columns.forEach((column) => {
+                rows[0].push(column[0]);
+                rows[1].push(column[1]);
+                rows[2].push(column[2]);
+                rows[3].push(column[3]);
+                rows[4].push(column[4]);
+            });
+
+
+            for(let i = 0; i < rows.length; i++) {
+                let statuses = rows[i].map((square) => square.status);
+                check = statuses.every(isTrue);
+                if(check) break;
+            }
+
+            return check;
+        }
+
+        const checkDiagonals = (board) => {
+            let columns = board.columns;
+            let diagonals = [[], []];
+            let check = false;
+
+            for(let i = 0; i < columns.length; i++) {
+                diagonals[0].push(columns[i][i]);
+                diagonals[1].push(columns[columns.length - (i + 1)][i])
+            }
+
+            for(let i = 0; i < diagonals.length; i++) {
+                let statuses = diagonals[i].map((square) => square.status);
+                check = statuses.every(isTrue);
+                if(check) break;
+            }
+
+            return check;
+        }
+
+        for(let i = 0; i < this.boards.length; i++) {
+            let columnWin = checkColumns(this.boards[i]);
+            let rowWin = checkRows(this.boards[i]);
+            let diagonalWin = checkDiagonals(this.boards[i]);
+
+            if(columnWin || rowWin || diagonalWin) return this.boards[i].player;
+            
+            return false;
+        }
+    }
+
+    play() {
+        this.generateBoards();
+        while(!this.gameWon) { this.drawNumber(); }
+    }
 }
 
 class Board {
@@ -88,6 +163,8 @@ class Board {
         }
         return columns;
     }
+
+    displayBoard() { console.log(this.columns); }
 }
 
 class Square {
@@ -97,13 +174,9 @@ class Square {
     }
 }
 
-let players = ['Harry Potter', 'Hermione Granger', 'Ron Weasley'];
+let players = ['Hermione Granger', 'Ron Weasley'];
 let game = new Game(players);
-game.generateBoards(1);
-while(!game.gameWon) {
-    game.drawNumber();
-}
-
+game.play();
 
 
  
